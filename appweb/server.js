4a59d53158e28e4f8979ecf5a818d67f29cda193
Ratz123323 <nathan.pierrot7@etu.univ-lorraine.fr>
2025-02-13 14:03:33 +0100

    restructuration fichiers

--- appweb/server.js
+++ appweb/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,21 +8,19 @@ require('dotenv').config();
 // Middleware pour parser le JSON
 app.use(express.json());
 
+//Configure le dossier 'html' qui contient les pages
+app.use(express.static(path.join(__dirname, 'html')));
+
 // Routes d'authentification
-const authRoutes = require('./src/routes/auth');
+const authRoutes = require('./src/backend/routes/auth');
 app.use('/api/auth', authRoutes);
 
-const adminRoutes = require('./src/routes/admin');
+const adminRoutes = require('./src/backend/routes/admin');
 app.use('/api/admin', adminRoutes);
 
 // Routes Back-office
-const productRoutes = require('./src/routes/product');
+const productRoutes = require('./src/backend/routes/product');
 app.use('/backoffice/products', productRoutes);
-
-
-// Importer les routes
-
-app.use('/api/besoins', besoinsRoutes);
 
 // Route racine pour vérifier que le serveur fonctionne
 app.get('/', (req, res) => {
